610419050ee7646904fdd75848f01151e6d80b4d
juan <juan191322@gmail.com>
2024-08-31 20:18:25 -0500

    Situacion Militar

---
+++ src/main/java/com/mycompany/situacion_militar/Situacion_militar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.situacion_militar;
+
+/**
+ *
+ * @author user
+ */
+public class Situacion_militar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
